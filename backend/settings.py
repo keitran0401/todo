4e7ea17dfc9fdd71cd05f546750ab665495c1199
Nghia Tran <thnghia0401@gmail.com>
2022-04-21 18:45:00 -0600

    fix heroku push x24

--- backend/settings.py
+++ backend/settings.py
@@ -17,8 +17,9 @@ import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+# dotenv_path = join(dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
