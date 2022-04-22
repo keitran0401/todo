67eb1be0bf673fddc97930a4c18b34f00f3b2467
Nghia Tran <thnghia0401@gmail.com>
2022-04-21 18:32:58 -0600

    fix heroku push x22

--- manage.py
+++ manage.py
@@ -2,16 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_dotenv
-from os.path import join, dirname
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-    dotenv_path = join(dirname(__file__), '.env')
-    load_dotenv(dotenv_path)
-
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
